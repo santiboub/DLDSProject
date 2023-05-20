aa990d3f0f9d700c93ae7a71e896ec3ded1a6cbb
Viktor Frideen <frideen.viktor@gmail.com>
2023-05-20 13:10:26 +0200

    Change initial_block

--- senet.py
+++ senet.py
@@ -121,7 +121,7 @@ class SENet(nn.Module):
         self.in_planes = 64
 
         self.initial_block = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
         )
@@ -130,7 +130,7 @@ class SENet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, k_list=k_list[sum(num_blocks[0:2]):sum(num_blocks[0:3])])
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, k_list=k_list[sum(num_blocks[0:3]):sum(num_blocks[0:4])])
         self.linear = nn.Linear(512 * block.expansion, num_classes)
-        self.avgp = nn.AvgPool2d(4)
+        self.avgp = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout2d(dropout)
 
     def forward(self, x):
